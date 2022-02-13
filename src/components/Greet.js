ffb844004cd706aa4bcdb5666f62b7543ca1a013
Suman Hansada <suman.hansada@gmail.com>
2022-02-13 17:55:04 +0530

    Added info about state and props

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -2,6 +2,22 @@ import React from "react";
 
 const Greet = (props) => {
   // You cannot change props under any circumstances (Immutable)
+  // In function component, you have to use props to access props
+  /**
+   * props
+   * - props get passed to the component
+   * - props are immutable (cannot be changed)
+   * - props (Functional Component)
+   * - this.props (Class Component)
+   */
+
+  /**
+   * state
+   * - state is a managed within the component
+   * - state is mutable (can be changed)
+   * - use useState Hook to manage state (Functional Component)
+   * - use this.state to access state (Class Component)
+   */
   console.log(props);
   return (
     <div>
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,7 +2,22 @@ import React, { Component } from "react";
 
 export class Welcome extends Component {
   // You cannot change props under any circumstances (Immutable)
-  // In class component, you have to use this.props
+  // In class component, you have to use this.props to access props
+  /**
+   * props
+   * - props get passed to the component
+   * - props are immutable (cannot be changed)
+   * - props (Functional Component)
+   * - this.props (Class Component)
+   */
+
+  /**
+   * state
+   * - state is a managed within the component
+   * - state is mutable (can be changed)
+   * - use useState Hook to manage state (Functional Component)
+   * - use this.state to access state (Class Component)
+   */
   render() {
     console.log(this.props);
     return (
