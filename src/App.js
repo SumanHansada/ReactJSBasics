cbe49795aacf3ceb073087d790059338bb95a475
Suman Hansada <suman.hansada@gmail.com>
2022-02-26 07:26:36 +0530

    React styling and CSS basics

--- src/App.js
+++ src/App.js
@@ -11,11 +11,19 @@ import EventBind from "./components/EventBind";
 import ParentComponent from "./components/ParentComponent";
 import UserGreeting from "./components/UserGreeting";
 import NamesList from "./components/NamesList";
+import Stylesheet from "./components/Stylesheet";
+import Inline from "./components/Inline";
+import "./appStyles.css";
+import styles from "./appStyles.module.css";
 
 function App() {
   return (
     <div className="App">
-      <NamesList />
+      <h1 className="error">Error</h1>
+      <h1 className={styles.success}>Success</h1>
+      <Inline />
+      <Stylesheet primary={true} />
+      {/* <NamesList /> */}
       {/* <UserGreeting /> */}
       {/* <ParentComponent /> */}
       {/* <EventBind /> */}
